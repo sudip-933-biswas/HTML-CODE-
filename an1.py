647a6fc60274c3c3c4cb85fdae4545fd63568140
sudip-123-biswas <sudipklm060@gmail.com>
2026-07-21 10:28:22 +0530

    Initial commit

---
+++ an1.py
@@ -0,0 +1,9 @@
+class student:
+    total_std=20
+    def set_Student(self,id,name):
+        self.id=id
+        self.name=name
+    
+    def get_student(self):
+        print(self.id)
+        print(self.name)
